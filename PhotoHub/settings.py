5f8e4c61defb05c0f5f6485c5b4bc5e72e54acff
Rohit Jha <98236703+Rohit6010@users.noreply.github.com>
2022-04-01 23:38:14 +0530

    Update hosts

--- PhotoHub/settings.py
+++ PhotoHub/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jn@^2fen2n(5v1zu#u)r22=0uzj+xa5@(v*%s(z@pc$(p)4#s4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['bicaption.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
